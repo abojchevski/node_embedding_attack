a5ba33c61b2e9da940af03a8b285cc1d71d743bf
abojchevski <aleksandar.bojchevski@gmail.com>
2019-05-13 21:02:12 +0200

    update copyright

--- __init__.py
+++ __init__.py
@@ -3,6 +3,4 @@ Implementation of the method proposed in the paper:
 'Adversarial Attacks on Node Embeddings'
 
 Copyright (C) owned by the authors, 2019
-Paper under review at ICML 2019. Do not distribute.
-Use granted only for reviewing purposes.
 """
