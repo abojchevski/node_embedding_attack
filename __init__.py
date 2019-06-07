e2e0265267a4b6318b41d3bfb64ec390f849df44
abojchevski <aleksandar.bojchevski@gmail.com>
2019-06-07 15:24:05 +0200

    Update copyright.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 """
 Implementation of the method proposed in the paper:
-'Adversarial Attacks on Node Embeddings'
+
+'Adversarial Attacks on Node Embeddings via Graph Poisoning'
+Aleksandar Bojchevski and Stephan Günnemann, ICML 2019
+http://proceedings.mlr.press/v97/bojchevski19a.html
 
 Copyright (C) owned by the authors, 2019
 """
--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 """
 Implementation of the method proposed in the paper:
-'Adversarial Attacks on Node Embeddings'
+
+'Adversarial Attacks on Node Embeddings via Graph Poisoning'
+Aleksandar Bojchevski and Stephan Günnemann, ICML 2019
+http://proceedings.mlr.press/v97/bojchevski19a.html
 
 Copyright (C) owned by the authors, 2019
 """
+
 import warnings
 
 import numpy as np
